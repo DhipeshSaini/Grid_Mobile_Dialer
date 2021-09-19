9c46744452c06c5d512a7f12339b75d88b30e524
DhipeshSaini <kdhipeshkomal@yahoo.com>
2021-09-19 13:50:12 +0530

    First Commit

---
+++ App2/App2/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var enteredNumber = (sender as Button).Text;
+            this.phoneNumber.Text += enteredNumber;
+
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+
+            //Logic to make a call
+        }
+    }
+}
